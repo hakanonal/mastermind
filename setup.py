b6bcdddf6555ab371d273ad2308645c099c4f8dd
Hakan Önal <hakan.onal@yahoo.com>
2020-12-07 23:53:33 +0300

    changed the package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mastermind-hakanonal", # Replace with your own username
+    name="mastermind",
     version="0.0.1",
     author="Hakan Onal",
     author_email="hakan.onal@yahoo.com",
